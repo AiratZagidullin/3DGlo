0ff7296f39a37bb32402952d59f58a817bb25481
Airat Zagidullin <zagbomar@gmail.com>
2022-06-23 16:21:56 +0500

    Выполнил обязательное задание 21

---
+++ src/modules/validation.js
@@ -0,0 +1,27 @@
+const validation = () => {
+    const priceInputs = document.querySelectorAll('div.calc-block>input')
+    const nameInput = document.querySelectorAll('input[name=user_name]')
+    const emailInput = document.querySelectorAll('input[name=user_email]')
+    const phoneInput = document.querySelectorAll('input[name=user_phone]')
+    const messInput = document.querySelector('input[name=user_message]')
+
+    const valid = (element, reg) => [
+        element.forEach(input => {
+            input.addEventListener('input', (e) => {
+                e.target.value = e.target.value.replace(reg, "")
+            })
+        })
+    ]
+
+    messInput.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я\s\-]+/, "")
+    })
+
+    valid(priceInputs, /\D+/)
+    valid(nameInput, /[^а-яА-Я\s\-]+/)
+    valid(emailInput, /[^\@\-\.\!\~\*\'\w]+/)
+    valid(phoneInput, /[^\(\)\-\d]+/)
+
+}
+
+export default validation
